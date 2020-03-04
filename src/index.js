24ad86a5a556e012a964125ffe2823db363a940c
Justin Karjoo <justin800k@gmail.com>
2020-03-04 12:22:00 -0600

    Added dynamic rendering for task modal

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import { renderListItem } from './list.js';
 import { renderTaskHeader } from './taskheader.js';
 import { renderTaskItem } from './task.js';
 import { renderListModal } from './listmodal.js';
+import { renderTaskModal } from './taskmodal.js';
 
 renderTaskHeader('School');
 renderTaskItem('Prog I Assignment', 'March 1st, 2020');
 renderTaskItem('Psych Research Paper', 'March 5th, 2020');
 renderTaskItem('Todo List Project', 'March 9th, 2020');
 
-renderListModal();
+
+
 /*
+renderListModal();
+renderTaskModal();
 renderListHeader();
 
 renderListItem('School');
 renderListItem('Groceries');
-renderListItem('Work');*/
+renderListItem('Work');
+
+*/
 
 
 
